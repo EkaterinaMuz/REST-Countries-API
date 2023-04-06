479d6f401fc9e274f65110370984e9b366f293c0
Ekaterina Muzheva <ekaterina.muzheva11@gmail.com>
2023-04-07 01:18:10 +0800

    route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header />
       <Main>
-       <HashRouter basename="/">
+       <HashRouter>
           <Routes>
             <Route path="/REST-Countries-API" element={
               <HomePage />
